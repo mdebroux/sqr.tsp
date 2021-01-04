bddf55bbfa64aa52c34a3a8c1813e2d83ec26e0f
mdebroux <56992550+mdebroux@users.noreply.github.com>
2021-01-04 14:50:22 +0100

    complétion des tests dans TourTest

--- src/test/java/tsp/lee/jacobson/TourTest.java
+++ src/test/java/tsp/lee/jacobson/TourTest.java
@@ -4,11 +4,59 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class TourTest {
+import java.util.ArrayList;
 
+public class TourTest {
+	/**
+	 * GIVEN WHEN THEN
+	 */
+
+	ArrayList path = new ArrayList();
+	City A = new City(0,0);
+	City B = new City(3,4);
+	City C = new City(1, 1);
+
+
+	@Test
+	public void getDistance_emptyTour_0() {
+		Tour emptyTour = new Tour();
+		int expected = 0;
+		int actual = emptyTour.getDistance();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getDistance_oneCity_0() {
+		path.add(B);
+		Tour tour = new Tour(path);
+		int expected = 0;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void getDistance_AB_10() {
+		path.add(A);
+		path.add(B);
+		Tour tour = new Tour(path);
+		int expected = 10;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getDistance_ABC_9() {
+		path.add(A);
+		path.add(B);
+		path.add(C);
+		Tour tour = new Tour(path);
+		int expected = 9;
+		int actual = tour.getDistance();
+		assertEquals(expected, actual);
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+
 
 }
